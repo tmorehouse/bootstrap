115920c85d3df9cac7a54fa55aaae7495faa54e1
Johann-S <johann.servoire@gmail.com>
2017-11-10 14:57:32 +0100

    Use Karma and ChromeHeadless to run our tests instead of PhantomJS

---
+++ karma.conf.js
@@ -0,0 +1,36 @@
+module.exports = function(config) {
+  const jQueryVersion = process.env.jqueryOld === 'true' ? 'js/tests/vendor/jquery-1.9.1.min.js' : 'assets/js/vendor/jquery-slim.min.js'
+  config.set({
+    frameworks: ['qunit'],
+    plugins: ['karma-chrome-launcher', 'karma-qunit'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      jQueryVersion,
+      'assets/js/vendor/popper.min.js',
+      'js/dist/util.js',
+      'js/dist/alert.js',
+      'js/dist/button.js',
+      'js/dist/carousel.js',
+      'js/dist/collapse.js',
+      'js/dist/dropdown.js',
+      'js/dist/modal.js',
+      'js/dist/tooltip.js',
+      'js/dist/popover.js',
+      'js/dist/scrollspy.js',
+      'js/dist/tab.js',
+      'js/tests/unit/*.js'
+    ],
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_ERROR || config.LOG_WARN,
+    autoWatch: false,
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    concurrency: Infinity
+  })
+}
